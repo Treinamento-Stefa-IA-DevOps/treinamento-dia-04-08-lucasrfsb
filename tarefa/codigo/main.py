27c4b8d3d7ed93d18bc66f31d11d5d0cee973cee
Lucas <lucas.oct8@gmail.com>
2021-08-09 23:04:25 -0300

    Add comments in main.py

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -13,9 +13,9 @@ def titanic(Sex:int, Age:float, Lifeboat: int,Pclass:int):
         data = [[Age, Lifeboat, Pclass, Sex]] ## from inspect_pickle.py discovered that inputs are sorted
 
         df = pd.DataFrame(data, columns = ['Age', 'Lifeboat', 'Pclass', 'Sex']) ## Rename to expected labeled columns
-        y = titanic.predict(df)
+        pred = titanic.predict(df) ## Calling predict method from DecisionTreeClassifier
 
-        ans = bool(y[0])
+        ans = bool(pred[0]) ## The first value is the prediction as the model expects
 
         return {
             "survived": ans,
